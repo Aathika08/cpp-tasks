3c7a8f6918d9256230dc5c944a3dbaba73b92dc5
aathika <83359584+Aathika08@users.noreply.github.com>
2021-11-28 22:07:24 +0530

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+ #include<iomanip>
+using namespace std;
+ 
+class Time
+{
+    private:
+        int seconds;
+        int hh,mm,ss;
+    public:
+        void getTime(void);
+        void convertIntoSeconds(void);
+        void displayTime(void);
+};
+ 
+void Time::getTime(void)
+{
+    cout << "Enter time:" << endl;
+    cout << "Hours?   ";          cin >> hh;
+    cout << "Minutes? ";          cin >> mm;
+    cout << "Seconds? ";          cin >> ss;
+}
+ 
+void Time::convertIntoSeconds(void)
+{
+    seconds = hh*3600 + mm*60 + ss;
+}
+ 
+void Time::displayTime(void)
+{
+    cout << "The time is = " << setw(2) << setfill('0') << hh << ":"
+                             << setw(2) << setfill('0') << mm << ":"
+                             << setw(2) << setfill('0') << ss << endl;
+    cout << "Time in total seconds: " << seconds;
+}
+ 
+int main()
+{
+    Time T; //creating objects
+     
+    T.getTime();
+    T.convertIntoSeconds();
+    T.displayTime();
+     
+    return 0;
+}
